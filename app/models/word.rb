98519543645b3f0fbe82dd71961d902ff8667790
chriskohlbrenner <c.kohlbrenner@gmail.com>
2014-04-01 14:54:26 -0400

    add translation method on words

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,22 +1,18 @@
 class Word < ActiveRecord::Base
   belongs_to :language
 
-  # def translate(original_lang, translation_lang)
-  #   google_url = "http://translate.google.com/translate_a/t?client=t&sl="
+  def translation
+    word_name = self.word
     
-  #   full_url = google_url + original_lang + "&tl=" + translation_lang + "&hl=pl&sc=2&ie=UTF-8&oe=UTF-8&prev=enter&ssel=0&tsel=0&"
+    begin
+      translate_hash = GoogleTranslate.new.translate("en","fr", word_name)
+      translations = translate_hash[1][0][1]
+      return_string = translations.join("<br>")
+    rescue
+      return_string = translate_hash[0][0][0]
+    end
+    
+    return_string ||= "translation not found"
+  end
 
-  #   uri = URI.parse(URI.escape(full_url))
-  #   http = Net::HTTP.new(uri.host, uri.port)
-  #   request = Net::HTTP::Post.new(uri.request_uri) 
-  #   request.set_form_data(text: self.word)
-  #   response = http.request(request)
-
-  #   # when trying to translate all, stuff breaks here - "incompatible character encodings: ASCII-8BIT and UTF-8"
-
-  #   to_parse = response.body.split(',').collect { |s| s == '' ? "\"\"" : s }.join(",") # fix json object
-  #   to_parse
-  #   result = JSON.parse(to_parse)
-  #   result[0][0][0]
-  # end
 end
