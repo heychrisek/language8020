c24700f1e1a45d53ee89b440a6e4fb5b94b2ae89
amcaplan <ariel.caplan@mail.yu.edu>
2014-03-31 23:28:04 -0400

    Further improve seeds.rb to automatically include all files in language_txt_files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,8 @@
-languages = ["English", "French", "Spanish", "Arabic", "Russian", "Chinese",
-  "Portuguese", "Italian", "Hebrew", "Afrikaans", "German", "Afrikaans",
-  "German", "Bulgarian"]
-
-languages.each do |language_name|
-  language = Language.create(name: language_name)
-  File.read("language_txt_files/#{language_name.downcase}.txt").gsub(/\d/, '').
+Dir.entries("language_txt_files").select{|file|file.end_with?('.txt')}.
+  each do |file|
+  language = Language.create(name: file[0..-5].capitalize)
+  File.read("language_txt_files/#{file}").gsub(/\d/, '').
     split("\n").reject(&:empty?)[0...1000].map(&:strip).each do |word|
-    word = Word.new(word: word)
-    word.language = language
-    word.save
+    Word.create(word: word, language: language)
   end
 end
