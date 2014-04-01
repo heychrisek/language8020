4eedd58d98b1fdad05e98905fa5a544b955e4274
amcaplan <ariel.caplan@mail.yu.edu>
2014-04-01 11:27:59 -0400

    Build up views to have proper next and previous links and paginate properly

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -4,20 +4,30 @@ class LanguagesController < ApplicationController
   end
 
   def show
-    params[:page] ||= 0
-    @page = params[:page]
-    params[:per_page] ||= 25
-    @per_page = params[:per_page]
-
-    @language = Language.find(params[:id])
-    @first_word_number = @page * @per_page + 1
-    @last_word_number = @first_word_number + @per_page - 1
-
-    first_id = @language.words.first.id
-    begin_range = first_id + @first_word_number - 1
-    end_range = first_id + @last_word_number
-    range = begin_range...end_range
-    @words = @language.words.find(range.to_a)
+    get_set_of_words
   end
 
+  def get_words
+    get_set_of_words
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+    def get_set_of_words
+      params[:page] ||= 0
+      @page = params[:page].to_i
+      params[:per_page] ||= 25
+      @per_page = params[:per_page].to_i
+
+      @language = Language.find(params[:id])
+      @first_word_number = @page * @per_page + 1
+      @last_word_number = @first_word_number + @per_page - 1
+
+      first_id = @language.words.first.id
+      @words = @language.words[(@first_word_number - 1)...@last_word_number]
+
+      redirect_to @language unless @words
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  get 'languages/' => "languages#index"
-  get 'languages/:id' => "languages#show", :as => "show"
+  resources :languages, only: [:index, :show]
+  # get 'languages/' => "languages#index"
+  # get 'languages/:id' => "languages#show", :as => "show"
   get 'languages/:id/words' => "languages#get_words", :as => "get_words"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'languages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
