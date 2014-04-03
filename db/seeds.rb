3d9dcc97bee4a6c621761d961d31d07a8261ec7b
chriskohlbrenner <c.kohlbrenner@gmail.com>
2014-04-03 09:52:37 -0400

    write script to have translations in text file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,34 @@
-Dir.entries("language_txt_files").select{|file|file.end_with?('.txt')}.
+# Seed words for each language from individual .txt files
+Dir.entries("word_txt_files").select{|file|file.end_with?('.txt')}.
   each do |file|
   language = Language.create(name: file[0..-5].capitalize)
-  File.read("language_txt_files/#{file}").gsub(/\d/, '').
+  File.read("word_txt_files/#{file}").gsub(/\d/, '').
     split("\n").reject(&:empty?)[0...1000].map(&:strip).each do |word|
     Word.create(word: word, language: language)
   end
 end
+
+# Seed transltions for each language from individual .txt files
+Dir.entries("translation_txt_files").select{|file|file.end_with?('.txt')}.
+  each do |file|
+  language = Language.find_by(name: file[0..-5].capitalize)
+  words = Word.where(language_id: language.id)
+  translations_from_file = File.read("translation_txt_files/#{file}").gsub(/\d/, '').split("\n").reject(&:empty?)[0...1000].map(&:strip)
+    # Have words and translations for a given language
+    # Need to update *each* word to have *each* translation
+
+    # Word.create(translation: translation_from_file, language: language)
+  # end
+end
+
+(1..5347).each do |i|
+  word = Word.find(i)
+  language = word.language
+  file = File.open("translation_txt_files/#{language.name.downcase}.txt", "a") do |f|
+    f << "#{word.translation}\n"
+  end
+end
+
 
 {
   "Afrikaans" => "af",
@@ -30,8 +53,7 @@ end
 end
 
 
-# To seed the translations of each word (remember to uncomment word.rb translation method)
-(5348..13954).each do |i|
-  word = Word.find(i)
-  word.update(:translation => word.translation)
-end
+# (5348..13954).each do |i|
+#   word = Word.find(i)
+#   word.update(:translation => word.translation)
+# end
